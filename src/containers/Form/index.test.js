705cbf41d74acfc724a67d9922f3f9a07384486f
Nirina VELOSONINA <vnirina081@gmail.com>
2023-09-12 12:40:05 +0200

    suppression du waitfor du En cours et Message envoyé

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -1,15 +1,13 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Form from "./index";
 
 describe("When Form is created", () => {
   it("a list of input fields is displayed", async () => {
     render(<Form />);
-    await waitFor(() => {
       expect(screen.getByText("Email")).toBeInTheDocument();
       expect(screen.getByText("Nom")).toBeInTheDocument();
       expect(screen.getByText("Prénom")).toBeInTheDocument();
       expect(screen.getByText("Personel / Entreprise")).toBeInTheDocument();
-    });
   });
 
   describe("and a click is triggered on the submit button", () => {
@@ -19,9 +17,9 @@ describe("When Form is created", () => {
       
       fireEvent.click(screen.getByTestId("button-test-id"));
       
-      await waitFor(() => {
+      await screen.findByText ("En cours");
+      await screen.findByText ("Envoyer");
       expect(onSuccess).toHaveBeenCalled();
-    });
   });
 });
 });
