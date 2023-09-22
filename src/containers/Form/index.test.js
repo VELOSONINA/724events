4a84ea8c3b524a06fd6943c2e5d6faba69215df7
Nirina VELOSONINA <vnirina081@gmail.com>
2023-09-23 00:27:40 +0200

    enlever le waitfor dans le test

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -1,15 +1,13 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen} from "@testing-library/react";
 import Form from "./index";
 
 describe("When Form is created", () => {
   it("a list of input fields is displayed", async () => {
     render(<Form />);
-    await waitFor(() => {
       expect(screen.getByText("Email")).toBeInTheDocument();
       expect(screen.getByText("Nom")).toBeInTheDocument();
       expect(screen.getByText("Prénom")).toBeInTheDocument();
       expect(screen.getByText("Personel / Entreprise")).toBeInTheDocument();
-    });
   });
 
   describe("and a click is triggered on the submit button", () => {
